01dcdbb01a8435f73c0f4667ea9e45d8df7aa9fe
pctech <pctech@PCTECH-PC>
2024-05-16 22:25:48 +0800

    Add project files.

---
+++ TicTacToe/Form1.Designer.vb
@@ -0,0 +1,261 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.Button6 = New System.Windows.Forms.Button()
+        Me.Button7 = New System.Windows.Forms.Button()
+        Me.Button8 = New System.Windows.Forms.Button()
+        Me.Button9 = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.LabelPlayerX = New System.Windows.Forms.Label()
+        Me.LabelPlayerO = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.ButtonReset = New System.Windows.Forms.Button()
+        Me.ButtonNewGame = New System.Windows.Forms.Button()
+        Me.ButtonExit = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.BackColor = System.Drawing.Color.White
+        Me.Button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button1.Font = New System.Drawing.Font("Microsoft Sans Serif", 36.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button1.Location = New System.Drawing.Point(150, 67)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(70, 70)
+        Me.Button1.TabIndex = 0
+        Me.Button1.UseVisualStyleBackColor = False
+        '
+        'Button2
+        '
+        Me.Button2.BackColor = System.Drawing.Color.White
+        Me.Button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button2.Font = New System.Drawing.Font("Microsoft Sans Serif", 36.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button2.Location = New System.Drawing.Point(226, 67)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(70, 70)
+        Me.Button2.TabIndex = 1
+        Me.Button2.UseVisualStyleBackColor = False
+        '
+        'Button3
+        '
+        Me.Button3.BackColor = System.Drawing.Color.White
+        Me.Button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button3.Font = New System.Drawing.Font("Microsoft Sans Serif", 36.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button3.Location = New System.Drawing.Point(302, 67)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(70, 70)
+        Me.Button3.TabIndex = 2
+        Me.Button3.UseVisualStyleBackColor = False
+        '
+        'Button4
+        '
+        Me.Button4.BackColor = System.Drawing.Color.White
+        Me.Button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button4.Font = New System.Drawing.Font("Microsoft Sans Serif", 36.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button4.Location = New System.Drawing.Point(150, 143)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(70, 70)
+        Me.Button4.TabIndex = 4
+        Me.Button4.UseVisualStyleBackColor = False
+        '
+        'Button5
+        '
+        Me.Button5.BackColor = System.Drawing.Color.White
+        Me.Button5.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button5.Font = New System.Drawing.Font("Microsoft Sans Serif", 36.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button5.Location = New System.Drawing.Point(226, 143)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(70, 70)
+        Me.Button5.TabIndex = 3
+        Me.Button5.UseVisualStyleBackColor = False
+        '
+        'Button6
+        '
+        Me.Button6.BackColor = System.Drawing.Color.White
+        Me.Button6.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button6.Font = New System.Drawing.Font("Microsoft Sans Serif", 36.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button6.Location = New System.Drawing.Point(302, 143)
+        Me.Button6.Name = "Button6"
+        Me.Button6.Size = New System.Drawing.Size(70, 70)
+        Me.Button6.TabIndex = 6
+        Me.Button6.UseVisualStyleBackColor = False
+        '
+        'Button7
+        '
+        Me.Button7.BackColor = System.Drawing.Color.White
+        Me.Button7.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button7.Font = New System.Drawing.Font("Microsoft Sans Serif", 36.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button7.Location = New System.Drawing.Point(150, 219)
+        Me.Button7.Name = "Button7"
+        Me.Button7.Size = New System.Drawing.Size(70, 70)
+        Me.Button7.TabIndex = 5
+        Me.Button7.UseVisualStyleBackColor = False
+        '
+        'Button8
+        '
+        Me.Button8.BackColor = System.Drawing.Color.White
+        Me.Button8.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button8.Font = New System.Drawing.Font("Microsoft Sans Serif", 36.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button8.Location = New System.Drawing.Point(226, 219)
+        Me.Button8.Name = "Button8"
+        Me.Button8.Size = New System.Drawing.Size(70, 70)
+        Me.Button8.TabIndex = 7
+        Me.Button8.UseVisualStyleBackColor = False
+        '
+        'Button9
+        '
+        Me.Button9.BackColor = System.Drawing.Color.White
+        Me.Button9.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button9.Font = New System.Drawing.Font("Microsoft Sans Serif", 36.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button9.Location = New System.Drawing.Point(302, 219)
+        Me.Button9.Name = "Button9"
+        Me.Button9.Size = New System.Drawing.Size(70, 70)
+        Me.Button9.TabIndex = 8
+        Me.Button9.UseVisualStyleBackColor = False
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 27.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(2, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(169, 42)
+        Me.Label1.TabIndex = 9
+        Me.Label1.Text = "Player X:"
+        '
+        'LabelPlayerX
+        '
+        Me.LabelPlayerX.AutoSize = True
+        Me.LabelPlayerX.Font = New System.Drawing.Font("Microsoft Sans Serif", 27.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.LabelPlayerX.Location = New System.Drawing.Point(168, 9)
+        Me.LabelPlayerX.Name = "LabelPlayerX"
+        Me.LabelPlayerX.Size = New System.Drawing.Size(39, 42)
+        Me.LabelPlayerX.TabIndex = 10
+        Me.LabelPlayerX.Text = "0"
+        '
+        'LabelPlayerO
+        '
+        Me.LabelPlayerO.AutoSize = True
+        Me.LabelPlayerO.Font = New System.Drawing.Font("Microsoft Sans Serif", 27.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.LabelPlayerO.Location = New System.Drawing.Point(483, 9)
+        Me.LabelPlayerO.Name = "LabelPlayerO"
+        Me.LabelPlayerO.Size = New System.Drawing.Size(39, 42)
+        Me.LabelPlayerO.TabIndex = 12
+        Me.LabelPlayerO.Text = "0"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 27.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(314, 9)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(173, 42)
+        Me.Label3.TabIndex = 11
+        Me.Label3.Text = "Player O:"
+        '
+        'ButtonReset
+        '
+        Me.ButtonReset.Font = New System.Drawing.Font("Microsoft Sans Serif", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ButtonReset.Location = New System.Drawing.Point(9, 319)
+        Me.ButtonReset.Name = "ButtonReset"
+        Me.ButtonReset.Size = New System.Drawing.Size(165, 34)
+        Me.ButtonReset.TabIndex = 13
+        Me.ButtonReset.Tag = ""
+        Me.ButtonReset.Text = "Reset"
+        Me.ButtonReset.UseVisualStyleBackColor = True
+        '
+        'ButtonNewGame
+        '
+        Me.ButtonNewGame.Font = New System.Drawing.Font("Microsoft Sans Serif", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ButtonNewGame.Location = New System.Drawing.Point(180, 319)
+        Me.ButtonNewGame.Name = "ButtonNewGame"
+        Me.ButtonNewGame.Size = New System.Drawing.Size(165, 34)
+        Me.ButtonNewGame.TabIndex = 14
+        Me.ButtonNewGame.Tag = ""
+        Me.ButtonNewGame.Text = "New Game"
+        Me.ButtonNewGame.UseVisualStyleBackColor = True
+        '
+        'ButtonExit
+        '
+        Me.ButtonExit.Font = New System.Drawing.Font("Microsoft Sans Serif", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ButtonExit.Location = New System.Drawing.Point(351, 319)
+        Me.ButtonExit.Name = "ButtonExit"
+        Me.ButtonExit.Size = New System.Drawing.Size(165, 34)
+        Me.ButtonExit.TabIndex = 15
+        Me.ButtonExit.Tag = ""
+        Me.ButtonExit.Text = "Exit"
+        Me.ButtonExit.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.ActiveBorder
+        Me.ClientSize = New System.Drawing.Size(524, 361)
+        Me.Controls.Add(Me.ButtonExit)
+        Me.Controls.Add(Me.ButtonNewGame)
+        Me.Controls.Add(Me.ButtonReset)
+        Me.Controls.Add(Me.LabelPlayerO)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.LabelPlayerX)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Button9)
+        Me.Controls.Add(Me.Button8)
+        Me.Controls.Add(Me.Button6)
+        Me.Controls.Add(Me.Button7)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Button5)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Tic Tac Toe"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Button5 As Button
+    Friend WithEvents Button6 As Button
+    Friend WithEvents Button7 As Button
+    Friend WithEvents Button8 As Button
+    Friend WithEvents Button9 As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents LabelPlayerX As Label
+    Friend WithEvents LabelPlayerO As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents ButtonReset As Button
+    Friend WithEvents ButtonNewGame As Button
+    Friend WithEvents ButtonExit As Button
+End Class
---
+++ TicTacToe/Form1.vb
@@ -0,0 +1,267 @@
+﻿Public Class Form1
+    Dim checkForXorO As Boolean = False
+    Dim addOnetoScore As Integer = 0
+    Sub buttonEnabledfalse()
+        Button1.Enabled = False
+        Button2.Enabled = False
+        Button3.Enabled = False
+        Button4.Enabled = False
+        Button5.Enabled = False
+        Button6.Enabled = False
+        Button7.Enabled = False
+        Button8.Enabled = False
+        Button9.Enabled = False
+    End Sub
+
+    Sub checkForWin()
+        If Button1.Text = "X" And Button2.Text = "X" And Button3.Text = "X" Then
+            Button1.BackColor = Color.Black
+            Button2.BackColor = Color.Black
+            Button3.BackColor = Color.Black
+            MessageBox.Show("Winner is Player X", "Tic Tac Toe", MessageBoxButtons.OK, MessageBoxIcon.Information)
+            addOnetoScore = Convert.ToInt64(LabelPlayerX.Text)
+            LabelPlayerX.Text = Convert.ToString(addOnetoScore + 1)
+            buttonEnabledfalse()
+        End If
+
+        If Button4.Text = "X" And Button5.Text = "X" And Button6.Text = "X" Then
+            Button4.BackColor = Color.Black
+            Button5.BackColor = Color.Black
+            Button6.BackColor = Color.Black
+            MessageBox.Show("Winner is Player X", "Tic Tac Toe", MessageBoxButtons.OK, MessageBoxIcon.Information)
+            addOnetoScore = Convert.ToInt64(LabelPlayerX.Text)
+            LabelPlayerX.Text = Convert.ToString(addOnetoScore + 1)
+            buttonEnabledfalse()
+        End If
+
+        If Button7.Text = "X" And Button8.Text = "X" And Button9.Text = "X" Then
+            Button7.BackColor = Color.Black
+            Button8.BackColor = Color.Black
+            Button9.BackColor = Color.Black
+            MessageBox.Show("Winner is Player X", "Tic Tac Toe", MessageBoxButtons.OK, MessageBoxIcon.Information)
+            addOnetoScore = Convert.ToInt64(LabelPlayerX.Text)
+            LabelPlayerX.Text = Convert.ToString(addOnetoScore + 1)
+            buttonEnabledfalse()
+        End If
+
+        If Button1.Text = "X" And Button4.Text = "X" And Button7.Text = "X" Then
+            Button1.BackColor = Color.Black
+            Button4.BackColor = Color.Black
+            Button7.BackColor = Color.Black
+            MessageBox.Show("Winner is Player X", "Tic Tac Toe", MessageBoxButtons.OK, MessageBoxIcon.Information)
+            addOnetoScore = Convert.ToInt64(LabelPlayerX.Text)
+            LabelPlayerX.Text = Convert.ToString(addOnetoScore + 1)
+            buttonEnabledfalse()
+        End If
+
+        If Button2.Text = "X" And Button5.Text = "X" And Button8.Text = "X" Then
+            Button2.BackColor = Color.Black
+            Button5.BackColor = Color.Black
+            Button8.BackColor = Color.Black
+            MessageBox.Show("Winner is Player X", "Tic Tac Toe", MessageBoxButtons.OK, MessageBoxIcon.Information)
+            addOnetoScore = Convert.ToInt64(LabelPlayerX.Text)
+            LabelPlayerX.Text = Convert.ToString(addOnetoScore + 1)
+            buttonEnabledfalse()
+        End If
+
+        If Button3.Text = "X" And Button6.Text = "X" And Button9.Text = "X" Then
+            Button3.BackColor = Color.Black
+            Button6.BackColor = Color.Black
+            Button9.BackColor = Color.Black
+            MessageBox.Show("Winner is Player X", "Tic Tac Toe", MessageBoxButtons.OK, MessageBoxIcon.Information)
+            addOnetoScore = Convert.ToInt64(LabelPlayerX.Text)
+            LabelPlayerX.Text = Convert.ToString(addOnetoScore + 1)
+            buttonEnabledfalse()
+        End If
+
+        If Button1.Text = "X" And Button5.Text = "X" And Button9.Text = "X" Then
+            Button1.BackColor = Color.Black
+            Button5.BackColor = Color.Black
+            Button9.BackColor = Color.Black
+            MessageBox.Show("Winner is Player X", "Tic Tac Toe", MessageBoxButtons.OK, MessageBoxIcon.Information)
+            addOnetoScore = Convert.ToInt64(LabelPlayerX.Text)
+            LabelPlayerX.Text = Convert.ToString(addOnetoScore + 1)
+            buttonEnabledfalse()
+        End If
+
+        If Button3.Text = "X" And Button5.Text = "X" And Button7.Text = "X" Then
+            Button3.BackColor = Color.Black
+            Button5.BackColor = Color.Black
+            Button7.BackColor = Color.Black
+            MessageBox.Show("Winner is Player X", "Tic Tac Toe", MessageBoxButtons.OK, MessageBoxIcon.Information)
+            addOnetoScore = Convert.ToInt64(LabelPlayerX.Text)
+            LabelPlayerX.Text = Convert.ToString(addOnetoScore + 1)
+            buttonEnabledfalse()
+        End If
+
+        'O Combinations
+
+        If Button1.Text = "O" And Button2.Text = "O" And Button3.Text = "O" Then
+            Button1.BackColor = Color.Black
+            Button2.BackColor = Color.Black
+            Button3.BackColor = Color.Black
+            MessageBox.Show("Winner is Player O", "Tic Tac Toe", MessageBoxButtons.OK, MessageBoxIcon.Information)
+            addOnetoScore = Convert.ToInt64(LabelPlayerO.Text)
+            LabelPlayerO.Text = Convert.ToString(addOnetoScore + 1)
+            buttonEnabledfalse()
+        End If
+
+        If Button4.Text = "O" And Button5.Text = "O" And Button6.Text = "O" Then
+            Button4.BackColor = Color.Black
+            Button5.BackColor = Color.Black
+            Button6.BackColor = Color.Black
+            MessageBox.Show("Winner is Player O", "Tic Tac Toe", MessageBoxButtons.OK, MessageBoxIcon.Information)
+            addOnetoScore = Convert.ToInt64(LabelPlayerO.Text)
+            LabelPlayerO.Text = Convert.ToString(addOnetoScore + 1)
+            buttonEnabledfalse()
+        End If
+
+        If Button7.Text = "O" And Button8.Text = "O" And Button9.Text = "O" Then
+            Button7.BackColor = Color.Black
+            Button8.BackColor = Color.Black
+            Button9.BackColor = Color.Black
+            MessageBox.Show("Winner is Player O", "Tic Tac Toe", MessageBoxButtons.OK, MessageBoxIcon.Information)
+            addOnetoScore = Convert.ToInt64(LabelPlayerO.Text)
+            LabelPlayerO.Text = Convert.ToString(addOnetoScore + 1)
+            buttonEnabledfalse()
+        End If
+
+        If Button1.Text = "O" And Button4.Text = "O" And Button7.Text = "O" Then
+            Button1.BackColor = Color.Black
+            Button4.BackColor = Color.Black
+            Button7.BackColor = Color.Black
+            MessageBox.Show("Winner is Player O", "Tic Tac Toe", MessageBoxButtons.OK, MessageBoxIcon.Information)
+            addOnetoScore = Convert.ToInt64(LabelPlayerO.Text)
+            LabelPlayerO.Text = Convert.ToString(addOnetoScore + 1)
+            buttonEnabledfalse()
+        End If
+
+        If Button2.Text = "O" And Button5.Text = "O" And Button8.Text = "O" Then
+            Button2.BackColor = Color.Black
+            Button5.BackColor = Color.Black
+            Button8.BackColor = Color.Black
+            MessageBox.Show("Winner is Player O", "Tic Tac Toe", MessageBoxButtons.OK, MessageBoxIcon.Information)
+            addOnetoScore = Convert.ToInt64(LabelPlayerO.Text)
+            LabelPlayerO.Text = Convert.ToString(addOnetoScore + 1)
+            buttonEnabledfalse()
+        End If
+
+        If Button3.Text = "O" And Button6.Text = "O" And Button9.Text = "O" Then
+            Button3.BackColor = Color.Black
+            Button6.BackColor = Color.Black
+            Button9.BackColor = Color.Black
+            MessageBox.Show("Winner is Player O", "Tic Tac Toe", MessageBoxButtons.OK, MessageBoxIcon.Information)
+            addOnetoScore = Convert.ToInt64(LabelPlayerO.Text)
+            LabelPlayerO.Text = Convert.ToString(addOnetoScore + 1)
+            buttonEnabledfalse()
+        End If
+
+        If Button1.Text = "O" And Button5.Text = "O" And Button9.Text = "O" Then
+            Button1.BackColor = Color.Black
+            Button5.BackColor = Color.Black
+            Button9.BackColor = Color.Black
+            MessageBox.Show("Winner is Player O", "Tic Tac Toe", MessageBoxButtons.OK, MessageBoxIcon.Information)
+            addOnetoScore = Convert.ToInt64(LabelPlayerO.Text)
+            LabelPlayerO.Text = Convert.ToString(addOnetoScore + 1)
+            buttonEnabledfalse()
+        End If
+
+        If Button3.Text = "O" And Button5.Text = "O" And Button7.Text = "O" Then
+            Button3.BackColor = Color.Black
+            Button5.BackColor = Color.Black
+            Button7.BackColor = Color.Black
+            MessageBox.Show("Winner is Player O", "Tic Tac Toe", MessageBoxButtons.OK, MessageBoxIcon.Information)
+            addOnetoScore = Convert.ToInt64(LabelPlayerO.Text)
+            LabelPlayerO.Text = Convert.ToString(addOnetoScore + 1)
+            buttonEnabledfalse()
+        End If
+
+    End Sub
+
+    Private Sub Button_Click(sender As Object, e As EventArgs) Handles Button9.Click, Button8.Click, Button7.Click, Button6.Click, Button5.Click, Button4.Click, Button3.Click, Button2.Click, Button1.Click
+        Dim b As Button = sender
+        If checkForXorO = False Then
+            b.Text = "X"
+            checkForXorO = True
+        Else
+            b.Text = "O"
+            checkForXorO = False
+        End If
+        checkForWin()
+        b.Enabled = False
+    End Sub
+
+    Private Sub ButtonReset_Click(sender As Object, e As EventArgs) Handles ButtonReset.Click
+        Button1.Enabled = True
+        Button2.Enabled = True
+        Button3.Enabled = True
+        Button4.Enabled = True
+        Button5.Enabled = True
+        Button6.Enabled = True
+        Button7.Enabled = True
+        Button8.Enabled = True
+        Button9.Enabled = True
+
+        Button1.Text = ""
+        Button2.Text = ""
+        Button3.Text = ""
+        Button4.Text = ""
+        Button5.Text = ""
+        Button6.Text = ""
+        Button7.Text = ""
+        Button8.Text = ""
+        Button9.Text = ""
+
+        Button1.BackColor = Color.White
+        Button2.BackColor = Color.White
+        Button3.BackColor = Color.White
+        Button4.BackColor = Color.White
+        Button5.BackColor = Color.White
+        Button6.BackColor = Color.White
+        Button7.BackColor = Color.White
+        Button8.BackColor = Color.White
+        Button9.BackColor = Color.White
+    End Sub
+
+    Private Sub ButtonNewGame_Click(sender As Object, e As EventArgs) Handles ButtonNewGame.Click
+        Button1.Enabled = True
+        Button2.Enabled = True
+        Button3.Enabled = True
+        Button4.Enabled = True
+        Button5.Enabled = True
+        Button6.Enabled = True
+        Button7.Enabled = True
+        Button8.Enabled = True
+        Button9.Enabled = True
+
+        Button1.Text = ""
+        Button2.Text = ""
+        Button3.Text = ""
+        Button4.Text = ""
+        Button5.Text = ""
+        Button6.Text = ""
+        Button7.Text = ""
+        Button8.Text = ""
+        Button9.Text = ""
+
+        Button1.BackColor = Color.White
+        Button2.BackColor = Color.White
+        Button3.BackColor = Color.White
+        Button4.BackColor = Color.White
+        Button5.BackColor = Color.White
+        Button6.BackColor = Color.White
+        Button7.BackColor = Color.White
+        Button8.BackColor = Color.White
+        Button9.BackColor = Color.White
+
+        LabelPlayerX.Text = "0"
+        LabelPlayerO.Text = "0"
+    End Sub
+
+    Private Sub ButtonExit_Click(sender As Object, e As EventArgs) Handles ButtonExit.Click
+        Dim checkforExit As DialogResult = MessageBox.Show("Confirm if you want to exit", "Tic Tac Toe", MessageBoxButtons.YesNo, MessageBoxIcon.Question)
+        If checkforExit = DialogResult.Yes Then
+            Application.Exit()
+        End If
+    End Sub
+End Class
